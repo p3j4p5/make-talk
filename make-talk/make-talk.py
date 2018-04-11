cd29edf5751f0ca2af2e35837e280d51ed053d1b
Paul Papacz <paulp@linx.net>
2018-04-11 23:57:13 +0200

    Add 'abs_path' method

--- make-talk/make-talk.py
+++ make-talk/make-talk.py
@@ -1,6 +1,7 @@
 """Creates a skelton for and manges a LaTeX Beamer presentation."""
 
 import argparse
+import os
 
 
 DESCRIPTION = (
@@ -111,6 +112,24 @@ def parse_args():
             args.date = '\\today'
 
     return args
+
+
+def abs_path(path):
+    """Return absolute path from given path string
+
+    Expands '~' and all '$' variables, and prepends the current working
+    directory in case the given path is relative.
+
+    Args:
+        path: String representing a (relative) path.
+
+    Returns:
+        String representing absolute path with all variables expanded.
+    """
+    if path:
+        path = os.path.realpath(os.path.expandvars(os.path.expanduser(path)))
+
+    return path
 
 
 if __name__ == '__main__':
