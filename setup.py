9ec704b47c6a73a98d11c3e21f110f76ad83d195
Paul Papacz <paulp@linx.net>
2018-05-10 22:23:10 +0100

    Dynamically determine paths for shared files ('data_files')

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
+import os
 from setuptools import setup
 
+PACKAGE_NAME = 'make_talk'
+
+DATA_SOURCE = 'share'
+
+DATA_FILES = [
+    (os.path.join(PACKAGE_NAME, top), [os.path.join(top, f) for f in files])
+    for top, dirs, files in os.walk(DATA_SOURCE)
+]
 
 setup(
-    name='make-talk',
+    name=PACKAGE_NAME,
     version='0.0.1',
     description='Creates a skelton for and manges a LaTeX Beamer presentation',
-    install_requires=['jinja2'],
-    entry_points={
+    ntry_points={
         'console_scripts': [
-            'make-talk = make_talk.shell:main'
+            'make-talk = %s.shell:main' % PACKAGE_NAME
         ]
-    }
+    },
+    data_files=DATA_FILES,
 )
